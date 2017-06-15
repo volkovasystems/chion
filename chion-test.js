8c1b242b13bdc98bc3cbd148c56e84842e58a57a
johnlenon <johnlenonmaghanoy@gmail.com>
2017-06-15 12:05:18 +0800

    Updated server/client test.

--- chion-test.js
+++ chion-test.js
@@ -6,8 +6,4 @@ assert.equal( chion( { } ), Object, "should be equal" );
 
 assert.equal( chion( Object ), Object, "should be equal" );
 
-assert.equal( chion( new Date( ) ), Date, "should be equal" );
-
-assert.equal( chion( Array ), Array, "should be equal" );
-
 console.log( "ok" );
--- chion.support.js
+++ chion.support.js
@@ -35,6 +35,10 @@
               			"module": "chion",
               			"author": "Richeve S. Bebedor",
               			"eMail": "richeve.bebedor@gmail.com",
+              			"contributors": [
+              				"John Lenon Maghanoy <johnlenonmaghanoy@gmail.com>",
+              				"Vinse Vinalon"
+              			],
               			"repository": "https://github.com/volkovasystems/chion.git",
               			"test": "chion-test.js",
               			"global": true
