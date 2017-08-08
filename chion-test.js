29bdd1554c917b58e3a120528f390163e303b22a
johnlenon <johnlenonmaghanoy@gmail.com>
2017-08-08 18:04:50 +0800

    Updated test.

--- chion-test.js
+++ chion-test.js
@@ -2,8 +2,29 @@
 const assert = require( "assert" );
 const chion = require( "./chion.js" );
 
-assert.equal( chion( { } ), Object, "should be equal" );
+assert.equal( chion( Object ), Object, "should be equal Object" );
 
-assert.equal( chion( Object ), Object, "should be equal" );
+class Orange {
+	constructor( ){
+		this.color = "orange";
+	}
+	getColor( ){
+		return "orange";
+	}
+}
+
+class Apple extends Orange {
+	constructor( ){
+		super( );
+
+		this.color = "red";
+	}
+	getColor( ){
+		return this.color;
+	}
+}
+
+let apple = new Apple( );
+assert.deepEqual( chion( apple ), apple.constructor, "should be equal to the constructor of the instance of Apple ");
 
 console.log( "ok" );
